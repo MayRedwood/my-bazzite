123ae18083bfb57b56ca177028e5f0272c56f253
May Munds <realmredwood@gmail.com>
2024-02-26 21:05:21 -0300

    Attempt number 5 at triple buffer

--- config/scripts/tribuffer.sh
+++ config/scripts/tribuffer.sh
@@ -5,20 +5,22 @@ set -oue pipefail
 wget https://copr.fedorainfracloud.org/coprs/trixieua/mutter-patched/repo/fedora-$(rpm -E %fedora)/trixieua-mutter-patched-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_trixieua-mutter-patched.repo
 
 # Reset overrides
-rpm-ostree override reset \
-  mutter \
-  mutter-common \
-  gnome-control-center \
-  gnome-control-center-filesystem \
-  gnome-shell \
-  xorg-x11-server-Xwayland
+# rpm-ostree override reset \
+#   mutter \
+#   mutter-common \
+#   gnome-control-center \
+#   gnome-control-center-filesystem \
+#   gnome-shell \
+#   xorg-x11-server-Xwayland
 # Apply our overrides
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched \
   gnome-shell \
   mutter \
   mutter-common \
   xorg-x11-server-Xwayland \
-  gdm
+  gdm \
+  gnome-control-center \
+  gnome-control-center-filesystem
 
 # Disable the COPR
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_trixieua-mutter-patched.repo
